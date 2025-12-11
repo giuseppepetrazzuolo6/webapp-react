9df5b2d30c3d436d5345bbc8342f525f7973b962
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-12-11 15:40:19 +0100

    add footer

---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,22 @@
+export default function Footer() {
+
+    return (
+        <div className="container">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
+                <div className="col">
+                    <h3>Movies Reviews App</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, dolorem.
+                    </p>
+                </div>
+                <div className="col">
+                    <h3>Menu</h3>
+                    <ul className="list-unstyled">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Contacts</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/assets/layouts/DefaultLayout.jsx
+++ src/assets/layouts/DefaultLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function DefaultLayout() {
 
@@ -9,7 +10,7 @@ export default function DefaultLayout() {
             <main className="vh-100">
                 <Outlet />
             </main>
-            <footer>footer</footer>
+            <Footer />
         </>
     )
 }
