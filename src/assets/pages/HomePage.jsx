7ec233b62ad3f722649b5863e128724abb246bb9
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-12-11 15:45:20 +0100

    Initialized movies state and added axios import in HomePage

--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -1,4 +1,10 @@
+import axios from "axios"
+import { useState } from "react"
+
 export default function HomePage() {
+
+    const [movies, setMovies] = useState([])
+
 
     return (
         <h1>movies</h1>
