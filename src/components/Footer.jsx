cefa92d076559a57fd666af8ab9019fa1f984012
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-12-13 17:11:43 +0100

    added content from the individual film to the MoviePage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,24 @@
 export default function Footer() {
 
     return (
-        <div className="container">
-            <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
-                <div className="col">
-                    <h3>Movies Reviews App</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, dolorem.
-                    </p>
-                </div>
-                <div className="col">
-                    <h3>Menu</h3>
-                    <ul className="list-unstyled">
-                        <li><a href="">Home</a></li>
-                        <li><a href="">Contacts</a></li>
-                    </ul>
+        <footer className="bg-light p-3 mt-3">
+            <div className="container">
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
+                    <div className="col">
+                        <h3>Movies Reviews App</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, dolorem.
+                        </p>
+                    </div>
+                    <div className="col">
+                        <h3>Menu</h3>
+                        <ul className="list-unstyled">
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Contacts</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
+        </footer>
     )
 }
--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,12 +1,42 @@
+import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom"
+import axios from "axios"
 
 export default function MoviePage() {
 
     const { id } = useParams()
+    const [movie, setMovie] = useState([])
+
+    useEffect(() => {
+        axios.get(`http://localhost:3000/api/movies/${id}`)
+            .then(response => {
+                console.log(response);
+                setMovie(response.data)
+            })
+            .catch(err => {
+                console.log(err.message);
+            })
+    }, [])
 
     return (
-        <div>
-            <h1>Movie Page: {id}</h1>
+        <div className="container mt-4">
+            <div className="row g-3">
+                <div className="col-12 col-md-4">
+                    <div className="card_image">
+                        <img src={`http://localhost:3000/${movie.image}`} alt=""
+                            style={{ width: '250px' }} />
+                    </div>
+                </div>
+                <div className="col-12 col-md-8">
+                    <h2 className="my-3">{movie?.title}</h2>
+                    <p><b>Director:</b> <em>{movie?.director}</em></p>
+                    <p><b>Genre:</b> <em>{movie?.genre}</em></p>
+                    <p><b>Release year:</b> <em>{movie?.release_year}</em></p>
+                    <p><b>Plot:</b> {movie?.abstract}</p>
+                    <Link className='btn btn-dark mt-5' to={`/`}>Back to Movies</Link>
+                </div>
+            </div>
         </div>
     )
 }
